eb38a082a59a53ab3b470fa39b5e8a2ea4328a3b
gegonmaz <gegonmaz@gmail.com>
2016-11-14 10:58:50 +0100

    Seguimos trabajando en el servidor Peer-to-Peer

--- server.py
+++ server.py
@@ -20,15 +20,24 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print("El cliente nos manda " + line.decode('utf-8'))
-            #Analizamos la linea para ver que metodo nos llega y contestar
+            # Analizamos la direccion IP y el PUERTO
+            IP = str(self.client_address[0])
+            print('Dir. IP del cliente: ' + IP)
+            PUERTO = str(self.client_address[1])
+            print('Puerto donde escucha cliente: ' + PUERTO)
+            # Analizamos la linea para ver que metodo nos llega y contestar 
             if argv[2] == 'INVITE':
-                self.wfile.write(b'SIP/2.0 200 Ok\r\n\r\n')
+            # INVITE --> tendremos que establecer la llamada(Comunicacion)                
                 self.wfile.write(b'SIP/2.0 100 Trying\r\n\r\n')
                 self.wfile.write(b'SIP/2.0 180 Ring\r\n\r\n')
+                self.wfile.write(b'SIP/2.0 200 Ok\r\n\r\n')
             if argv[2] == 'BYE':
-                self.wfile.write(b'SIP/2.0 200 Ok\r\n\r\n') 
+            # BYE --> se cierra la comunicación
+                self.wfile.write(b'SIP/2.0 200 Ok\r\n\r\n')
+            if not argv[2] == 'INVITE'|'BYE':
+                self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n\r\n') 
             else:
-                self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n\r\n')
+                self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
