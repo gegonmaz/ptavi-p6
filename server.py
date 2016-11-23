bac1205f112decfd654c9a50360c56398da1143c
gegonmaz <gegonmaz@gmail.com>
2016-11-23 21:46:19 +0100

    Intentado acabar el programa, salta un error de sintaxis que no se como solucionar. Seguimos intentandolo

--- server.py
+++ server.py
@@ -13,50 +13,46 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+    Metodos = ['INVITE', 'BYE', 'ACK']
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         IP_Cliente = str(self.client_address[0])
         PUERTO = int(self.client_address[1])
-        print('El cliente está escuchando en dirección ' + IP_CLiente + ' y puerto ' 
-                + PUERTO)
-        fichero_audio = sys.argv[3]
-        self.wfile.write(b"Hemos recibido tu peticion")
+        audio = sys.argv[3]
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print("El cliente nos manda " + line.decode('utf-8'))
             # Partimos el mensaje
             Mensaje_Cliente = line.decode('utf-8').split()
-            # Analizamos la linea para ver que metodo nos llega y contestar 
-            if Mensaje_Cliente[0] == 'INVITE':
-            # INVITE --> tendremos que establecer la llamada(Comunicacion)                
-                self.wfile.write(b'SIP/2.0 100 Trying\r\n\r\n')
-                self.wfile.write(b'SIP/2.0 180 Ring\r\n\r\n')
-                self.wfile.write(b'SIP/2.0 200 Ok\r\n\r\n')
-            elif Mensaje_CLiente[0] == 'BYE':
-                # BYE --> se cierra la comunicación
-                self.wfile.write(b'SIP/2.0 200 Ok\r\n\r\n')
-            elif Mensaje_CLiente[0] == 'ACK':
-                # aEjecutar es un string con lo que se ha de ejecutar en la shell
-                aEjecutar = ’mp32rtp -i 127.0.0.1 -p 23032 < ’ + fichero_audio
-                print "Vamos a ejecutar", aEjecutar
-                os.system(aEjecutar)
-
-            if not Mensaje_CLiente[0] == 'INVITE'|'BYE'|'ACK':
-                self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n\r\n') 
-            else:
-                self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
-            # Si no hay más líneas salimos del bucle infinito
+            # Analizamos la linea para ver que metodo nos llega y contestar
+            # Comprobamos el tamaño del paquete recibido
             if not line:
                 break
+            if Mensaje_Cliente[0] == 'INVITE':
+                # INVITE --> tendremos que establecer la llamada(Comunicacion)
+                envio = ("SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring\r\n\r\n" +
+                         "SIP/2.0 200 OK\r\n\r\n")
+                self.wflie.write(bytes(envio, 'utf-8')
+            elif Mensaje_Cliente[0] == 'ACK':
+                os.system("./mp32rtp -i " + IP_Cliente + " -p 23032 <" + audio)
+            elif Mensaje_Cliente[0] == 'BYE':
+                # BYE --> se cierra la comunicación
+                envio=self.wfile.write(b'SIP/2.0 200 Ok\r\n\r\n')
+            if not Mensaje_Cliente[0] in self.Metodos:
+                # if not Mensaje_Cliente[0] == 'INVITE'|'BYE'|'ACK':
+                envio=self.wfile.write(b'SIP/2.0 405 Method Not Allowed' +
+                                       b'\r\n\r\n')
+            else:
+                envio=self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     # serv = socketserver.UDPServer(('', 6001), EchoHandler)
     # print("Lanzando servidor UDP de eco...")
     """
-    Este es el programa para un servidor eco, pero ahora no queremos que el 
+    Este es el programa para un servidor eco, pero ahora no queremos que el
     servidor conteste lo le envia el cliente. Queremos las respuestas propias.
     """
     # Primero comprobaremos el tamaño del mensaje que nos llega.
@@ -64,16 +60,20 @@ if __name__ == "__main__":
         sys.exit("Usage: python server.py IP port audio_file")
     # Nos guardamos las variables de puerto y direccion IP del servidor
     try:
-        IP_Servidor = sys.argv[1]
-        PUERTO_Servidor = sys.argv[2]
+        IP_Servidor=sys.argv[1]
+        PUERTO_Servidor=sys.argv[2]
         """
-        Ademas ahora a parte del puerto y la direccion IP, encontramos      
-        el archivo de audio que enviamos por RTP              
+        Ademas ahora a parte del puerto y la direccion IP, encontramos
+        el archivo de audio que enviamos por RTP
         """
-        Archivo = sys.argv[3]
+        Archivo=sys.argv[3]
     except IndexError:
         sys.exit("Usage: python server.py IP port audio_file")
-    # Creamos un servidor y escuchamos 
-    serv = socketserver.UDPServer(("", int(PUERTO_Servidor)), EcoHandler)
-    print("Estamos escuchando en el puerto " +  PUERTO_Servidor)
+    # Creamos un servidor y escuchamos
+    try:
+        serv=socketserver.UDPServer(("", int(PUERTO_Servidor)), EchoHandler)
+    except ValueError:
+        sys.exit('El Puerto necesita un entero')
+
+    print("Estamos escuchando en el puerto " + PUERTO_Servidor)
     serv.serve_forever()
